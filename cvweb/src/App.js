a98a0f883be686b0905a543153f0c518ead5263a
tjabejohannes <tjabejv@stud.ntnu.no>
2019-08-22 22:55:41 +0200

    Making the p5 library work with react.

--- cvweb/src/App.js
+++ cvweb/src/App.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
-import Contact from './components/Contact';
+import Projects from "./components/Projects";
+import Playground from './components/Playground';
 import './App.css'
 
 
@@ -14,14 +15,16 @@ class App extends Component {
           <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
             <ul className="navbar-nav mr-auto">
               <li><Link to={'/'} className="nav-link">Home</Link></li>
-              <li><Link to={'/contact'} className="nav-link">Contact</Link></li>
               <li><Link to={'/about'} className="nav-link">About</Link></li>
+              <li><Link to={'/projects'} className="nav-link">Projects</Link></li>
+              <li><Link to={'/playground'} className="nav-link">Playground</Link></li>
             </ul>
           </nav>
           <Switch>
             <Route exact path='/' component={Home} />
-            <Route path='/contact' component={Contact} />
+            <Route path='/projects' component={Projects} />
             <Route path='/about' component={About} />
+            <Route path='/playground' component={Playground} />
           </Switch>
         </div>
       </Router>
---
+++ cvweb/src/components/Projects.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+export class Projects extends Component {
+    render() {
+        return (<div>
+            <h2>Projects</h2>
+        </div>);
+    }
+}
+
+
+
+export default Projects;
