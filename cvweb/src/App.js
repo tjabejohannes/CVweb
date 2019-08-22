7250afcbe44698b6e33b06eddd32295992c76e9a
tjabejohannes <tjabejv@stud.ntnu.no>
2019-08-22 16:21:39 +0200

    atlaskit init and cwweb router config

--- cvweb/src/App.js
+++ cvweb/src/App.js
@@ -1,18 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          The CV web application
-        </p>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div class='container-all'>
+          <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+            <ul className="navbar-nav mr-auto">
+              <li><Link to={'/'} className="nav-link">Home</Link></li>
+              <li><Link to={'/contact'} className="nav-link">Contact</Link></li>
+              <li><Link to={'/about'} className="nav-link">About</Link></li>
+            </ul>
+          </nav>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/contact' component={Contact} />
+            <Route path='/about' component={About} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
