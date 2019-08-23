465493d02cc7c386556aa2e767b3a1b107ca8d27
tjabejohannes <tjabejv@stud.ntnu.no>
2019-08-23 16:40:22 +0200

    Changing card layout on about-page

--- cvweb/src/components/About.jsx
+++ cvweb/src/components/About.jsx
@@ -18,23 +18,23 @@ class About extends Component {
         return (
             <StyledCardContainer>
                 <Card type="large">
-                    <BaseInformation name={this.state.name} tlf={this.state.tlf}></BaseInformation>
+                    <BaseInformation name={this.state.name} tlf={this.state.tlf} img="https://scontent-arn2-1.cdninstagram.com/vp/f2675ce4947d4a7d0133287212f84a9f/5DEFA134/t51.2885-19/s320x320/32430479_371055023406210_3021052246222700544_n.jpg?_nc_ht=scontent-arn2-1.cdninstagram.com"></BaseInformation>
                 </Card>
                 <Card>
-                    <h2>{this.state.name}</h2>
-                    <h3>{this.state.birthday}</h3>
+                    <h2>              .      </h2>
+                    <h2>              .      </h2>
+                    <h2>              .      </h2>
+                    <h2>              .      </h2>
+                    <h2>              .      </h2>
+                    <h2>              .      </h2>
                 </Card>
                 <Card>
-                    <h2>{this.state.name}</h2>
-                    <h3>{this.state.birthday}</h3>
-                </Card>
-                <Card>
-                    <h2>{this.state.name}</h2>
-                    <h3>{this.state.birthday}</h3>
-                </Card>
-                <Card>
-                    <h2>jkdsjadjflkjasldjfalkjs</h2>
-                    <h3>{this.state.birthday}</h3>
+                    <h2>              .      </h2>
+                    <h2>              .      </h2>
+                    <h2>              .      </h2>
+                    <h2>              .      </h2>
+                    <h2>              .      </h2>
+                    <h2>              .      </h2>
                 </Card>
             </StyledCardContainer>
         );
--- cvweb/src/components/BaseInformation.jsx
+++ cvweb/src/components/BaseInformation.jsx
@@ -33,7 +33,7 @@ class BaseInformation extends Component {
                     <h2>{this.props.name}</h2>
                 </div>
                 <div className="image">
-                    <img alt="profile" src="https://scontent-arn2-1.cdninstagram.com/vp/f2675ce4947d4a7d0133287212f84a9f/5DEFA134/t51.2885-19/s320x320/32430479_371055023406210_3021052246222700544_n.jpg?_nc_ht=scontent-arn2-1.cdninstagram.com" />
+                    <img alt="profile" src={this.props.img} />
                 </div>
             </StyledBaseInformation>
         );
