ba4b64b184f689e468a59205a9d3e3398dcb699b
tjabejohannes <tjabejv@stud.ntnu.no>
2019-08-26 13:38:19 +0200

    Updates

---
+++ precache-manifest.dcfdb9d86402f97aba1e94683d4fbdac.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f90de78f2f2f4762b98948152047160",
+    "url": "/CVweb/index.html"
+  },
+  {
+    "revision": "979d0895641e59a1aea3",
+    "url": "/CVweb/static/js/2.de9e0528.chunk.js"
+  },
+  {
+    "revision": "f3f765814cd57bcce04f",
+    "url": "/CVweb/static/js/main.e33d0cb0.chunk.js"
+  },
+  {
+    "revision": "b61d6ff591f004d31595",
+    "url": "/CVweb/static/js/runtime~main.4c2156fe.js"
+  }
+]);
