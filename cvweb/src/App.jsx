ed82f3f6eb1e1aaa5114c255da2eb4b3bd89bdc6
tjabejohannes <tjabejv@stud.ntnu.no>
2019-08-26 14:06:47 +0200

    Appending the CVweb to the Home route

--- cvweb/src/App.jsx
+++ cvweb/src/App.jsx
@@ -21,13 +21,13 @@ class App extends Component {
           <nav className="navbar navbar-expand fixed-top navbar-light bg-light">
 
             <ul className="navbar-nav">
-              <li className="nav-item"><Link to={'/'} className="nav-link">About Me</Link></li>
+              <li className="nav-item"><Link to={'/CVweb'} className="nav-link">About Me</Link></li>
               <li className="nav-item"><Link to={'/projects'} className="nav-link">Projects</Link></li>
               {/*<li><Link to={'/playground'} className="nav-link">Playground</Link></li>*/}
             </ul>
           </nav>
           <Switch>
-            <Route exact path='/' component={About} />
+            <Route exact path='/CVweb' component={About} />
             <Route path='/projects' component={Projects} />
             {/*<Route path='/playground' component={Playground} />*/}
           </Switch>
